8556b2032dcddfd3900ac4c66c5d8d72182935d1
Carlos Andres Hoyos <choyos2906@gmail.com>
2022-09-26 20:01:05 +0200

    Center board game

--- src/components/GridTable.vue
+++ src/components/GridTable.vue
@@ -2,14 +2,11 @@
   <div class="Connec4Layout">
     <table ref="table">
       <thead>
-        <th v-for="cell in columns" :key="cell">
-          <span v-if="cell == 1"> Player 1</span>
-          <span v-if="cell == 6"> Player 2</span>
-        </th>
+        <th v-for="cell in columns" :key="cell"></th>
       </thead>
       <tbody>
         <tr v-for="row in rows" :key="row">
-          <td v-for="body_cell in columns" :key="body_cell"> </td>
+          <td v-for="body_cell in columns" :key="body_cell"></td>
         </tr>
       </tbody>
     </table>
@@ -28,82 +25,23 @@ const rows = 7;
 
 <style>
 
-table {
-  background-color: blueviolet;
+#Connec4Layout {
+  
 }
-/*  th{
-    padding: 5px;
-  }
-*/
- td {
+table {
+  margin: auto;
+  background-color: blueviolet;
+  display: flex;
+  flex-direction: column;
+  max-width: 350px;
+  gap: 5px;
+}
+
+td {
   background-color: white;
   padding: 2.5px;
-  width: 80px;
-  height: 80px;
-  /* width: calc(100% / 6);
-  height: calc(100% / 6); */
+  width: 50px;
+  height: 50px;
   border-radius: 50%;
-} 
-/* 
-#Connect4Layout {
-  margin: 20px 0;
 }
-#Connect4Layout table {
-  min-width: 300px;
-  width: 100%;
-  height: fit-content;
-  overflow-y: scroll;
-  border-collapse: collapse;
-  border: 1px solid #000;
-}
-#Connect4Layout table tr td {
-  border: 1px solid #000;
-  padding: 10px;
-}
-#Connect4Layout table .bold {
-  border-right: none;
-  border-left: none;
-  background: #ddd;
-  border-bottom: 1px solid #c2c2c2;
-  border: none;
-}
-#Connect4Layout table .bold td {
-  border: none;
-  background: #000;
-  color: #fff;
-  border: none;
-  font-size: 18px;
-  font-weight: bold;
-}
-#Connect4Layout .with-header tr:first-of-type td {
-  border: none;
-}
-@media (max-width: 480px) {
-  #Connect4Layout .with-header tr:first-of-type {
-    display: none;
-  }
-  #Connect4Layout .with-header tr {
-    border: 1px solid #000;
-  }
-  #Connect4Layout .with-header tr td {
-    display: block;
-    border: none;
-  }
-  #Connect4Layout .with-header tr td:first-child {
-    padding-top: 0.5em;
-  }
-  #Connect4Layout .with-header tr td:last-child {
-    padding-bottom: 0.5em;
-  }
-  #Connect4Layout .with-header tr td:before {
-    content: attr(data-th) " : ";
-    font-weight: bold;
-    display: inline-block;
-  }
-}
-@media (max-width: 480px) and (min-width: 480px) {
-  #Connect4Layout .with-header tr td:before {
-    display: none;
-  }
-} */
 </style>
